5b153ec2e702dfc88e879f93a1311b24692ea7c0
minjujung <love053011@gmail.com>
2021-07-16 22:23:47 +0900

    Make Modal Component and Common Files

---
+++ src/pages/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Modal from "../components/Modal";
+
+const Main = (props) => {
+  return (
+    <>
+      <Modal width="40%" height="60%">
+        <h1>hello</h1>
+        <input type="text" />
+      </Modal>
+    </>
+  );
+};
+
+export default Main;
